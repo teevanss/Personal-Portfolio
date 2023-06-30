954b3d120db8277c52f5c64944467ba0defd4130
Taylor Evans <teevans@buffalo.edu>
2023-06-29 21:44:20 -0500

    Homepage mostly complete

---
+++ my-app/src/components/Banner.js
@@ -0,0 +1,51 @@
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { Container, Row, Col } from "react-bootstrap";
+import TrackVisibility from 'react-on-screen';
+import Typed from 'react-typed';
+import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+
+export const Banner = () => {
+
+  return ( 
+    
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Taylor`}<br></br>
+                <Typed strings={["Web Developer", "Web Designer", "UI/UX Designer"]} typeSpeed={70} backSpeed={40} backDelay={3000} loop/></h1>
+
+                <p>I'm a 23 year old graduate from the University at Buffalo. 
+                I've recently moved from New York to Texas! 
+                I have a Bachelor of Science degree in computer science. 
+                Web design combines my two favorite hobbies: coding and art!</p>
+
+                <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+
+            </TrackVisibility>
+          </Col>
+
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+
+            </TrackVisibility>
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { HashLink } from 'react-router-hash-link';
+import { BrowserRouter as Router } from "react-router-dom";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon1purple from "../assets/img/nav-icon1p.svg";
+import navIcon2purple from "../assets/img/nav-icon2p.svg";
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
+  {/* To change background color */}
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+
+          <Navbar.Brand href="/">
+            <h1>Taylor Evans</h1>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+
+            <span className="navbar-text">
+
+              <div className="social-icon" onMouseOver={() => setOpen(true)} onMouseOut={() => setOpen(false)}>
+                <a href="https://github.com/teevanss"><img src={open ? navIcon1purple : navIcon1} alt="GitHub link"/></a>
+              </div>
+              <div className="social-icon" onMouseOver={() => setOpen2(true)} onMouseOut={() => setOpen2(false)}>
+                <a href="https://www.linkedin.com/in/taylor-evans-0992b9236/"><img src={open2 ? navIcon2purple : navIcon2} alt="" /></a>
+              </div>
+
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let's Connect</span></button>
+              </HashLink>
+            </span>
+
+          </Navbar.Collapse>
+
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
