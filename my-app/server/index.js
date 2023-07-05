1d23b5b05ebc36f9c641bbfee86d8422bad654df
Taylor Evans <teevans@buffalo.edu>
2023-07-05 10:18:58 -0500

    Deployment fix

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -60,7 +60,7 @@ app.post("/api/contact", bodyParser.urlencoded({extended: false}), (req, res) =>
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', index.html))
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 }); 
 
 app.listen(PORT, () => {
